636e92a348c3ce3345f4e0570dc6952bf1c73b1f
fiouf42 <2bleyman@gmail.com>
2026-01-22 10:31:06 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '')
 
   return {
-    base: mode === 'production' ? '/bs-technologie/' : '/',
+    base: mode === 'production' ? '/BS---Technologie/' : '/',
     
     plugins: [react()],
     server: {
